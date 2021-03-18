71e206d406d2a7e0385f33b84d77bb4778a643de
Mayvis <kevin73911@gmail.com>
2021-03-18 12:16:17 +0800

    Import element-ui locale

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
 import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/zh-TW";
 import "element-ui/lib/theme-chalk/index.css";
 import PortalVue from "portal-vue";
 require("default-passive-events");
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 Vue.use(PortalVue);
 
 Vue.config.productionTip = false;
